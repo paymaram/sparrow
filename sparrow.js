d440c66331725e3271903fac355d55d5e0543ac5
00b11 <93651561+paymaram@users.noreply.github.com>
2023-04-03 18:53:39 +0530

    Update sparrow.js

--- sparrow.js
+++ sparrow.js
@@ -13,7 +13,9 @@ function DOMcard(){
 
 
 function MediaRes(){
+  list = [400,500,600,700,800,900,1000]
   for (var i=0, i++ ){
+     
     
     /*
     here add content to convert css into responsive 
